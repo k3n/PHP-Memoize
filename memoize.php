560b02b35eee750d4a1043d374d4b05c0d08d65e
k3n <github.com@kenman.net>
2012-11-04 01:32:27 -0500

    Update memoize.php
    Can now use array($obj, 'method') syntax for the hashing function.

--- memoize.php
+++ memoize.php
@@ -5,16 +5,15 @@
  * @version PHP 5.3 for anonymous function support (required)
  * @version PHP 5.4 for callable type-hint support (optional)
  *
- * @param callable $func function to optimize
- * @param callable $hash generates a hash (string) for the lookup key
- * @param callable $hash - the function to calculate the hash;
+ * @param callable $fnOrig function to optimize
+ * @param callable $fnHash function to generate the hash
  * @return function
  */
 function memoize(callable $fnOrig, callable $fnHash) {
     return function() use ($fnOrig, $fnHash) {
-        $args = func_get_args();
-        $key = $fnHash($args);
         static $hash = array();
+        $args = func_get_args();
+        $key = call_user_func($fnHash, $args);
         if (!array_key_exists($key, $hash)) {
             $hash[$key] = call_user_func_array($fnOrig, $args);
         }
