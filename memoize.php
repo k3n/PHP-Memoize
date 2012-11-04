42856d2b03e7778aed3a5dcdd9834963cf3d75e1
k3n <github.com@kenman.net>
2012-11-04 01:20:44 -0500

    Update memoize.php

--- memoize.php
+++ memoize.php
@@ -2,30 +2,22 @@
 /**
  * Wrap an existing function or method with a memoization implementation.
  *
- * This is a poor-man's in-memory cache, and plays on the classic time/space
- * tradeoff whereby we are trading memory space for computation time. The more
- * use your memoized function sees, the more memory will be used; however, if
- * there are repeated function calls that will yield the same result, then we
- * end up calling the original [now-wrapped] function less frequently than if
- * it had not been memoized.
+ * @version PHP 5.3 for anonymous function support (required)
+ * @version PHP 5.4 for callable type-hint support (optional)
  *
- * @param callable $func - the function to optimize
- * @param callable $hash - the function to calculate the hash; it will receive a
- *                         single argument: an array representing the parameters
- *                         passed to your function. Your hash function should
- *                         return a scalar value that can safely be used as an
- *                         array key.
+ * @param callable $func function to optimize
+ * @param callable $hash generates a hash (string) for the lookup key
+ * @param callable $hash - the function to calculate the hash;
  * @return function
  */
-function memoize(callable $func, callable $hash) {
-    return function() use ($func, $hash) {
+function memoize(callable $fnOrig, callable $fnHash) {
+    return function() use ($fnOrig, $fnHash) {
         $args = func_get_args();
-        $key = $hash($args);
-        static $cache = array();
-        if (!array_key_exists($key, $cache)) {
-            $cache[$key] = call_user_func_array($func, $args);
+        $key = $fnHash($args);
+        static $hash = array();
+        if (!array_key_exists($key, $hash)) {
+            $hash[$key] = call_user_func_array($fnOrig, $args);
         }
-        return $cache[$key];
+        return $hash[$key];
     };
 }
-?>
