2084ea668c9649f044048d23c54940dcdfc8937e
Jesse Donat <donatj@gmail.com>
2013-11-08 14:59:36 -0600

    Added a simple default hasher

--- memoize.php
+++ memoize.php
@@ -9,14 +9,20 @@
  * @param callable $fnHash function to generate the hash
  * @return function
  */
-function memoize(callable $fnOrig, callable $fnHash) {
-    return function() use ($fnOrig, $fnHash) {
-        static $hash = array();
-        $args = func_get_args();
-        $key = call_user_func($fnHash, $args);
-        if (!array_key_exists($key, $hash)) {
-            $hash[$key] = call_user_func_array($fnOrig, $args);
-        }
-        return $hash[$key];
-    };
+function memoize(callable $fnOrig, callable $fnHash = null) {
+	return function() use ($fnOrig, $fnHash) {
+		static $hash = array();
+		$args = func_get_args();
+
+		if( $fnHash === null ) {
+			$key = sha1(serialize($args));
+		}else{
+			$key = call_user_func($fnHash, $args);
+		}
+
+		if (!array_key_exists($key, $hash)) {
+			$hash[$key] = call_user_func_array($fnOrig, $args);
+		}
+		return $hash[$key];
+	};
 }
